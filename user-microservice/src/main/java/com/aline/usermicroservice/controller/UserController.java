49979b8b4a00ef53f6de17fbb9c3f5161e5baa40
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-09 13:36:48 -0600

    Update controller to use new Swagger3 annotations.

--- user-microservice/src/main/java/com/aline/usermicroservice/controller/UserController.java
+++ user-microservice/src/main/java/com/aline/usermicroservice/controller/UserController.java
@@ -4,12 +4,12 @@ import com.aline.core.dto.request.UserProfileUpdate;
 import com.aline.core.dto.request.UserRegistration;
 import com.aline.core.dto.response.PaginatedResponse;
 import com.aline.core.dto.response.UserResponse;
-import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api("/users")
+@Tag(name = "Users")
 @RestController
 public class UserController implements IUserController {
 
