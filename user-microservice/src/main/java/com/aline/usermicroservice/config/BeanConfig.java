23be527ffc495756c4989814d14de7f24d7f6241
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-10 14:32:09 -0600

    Restart create UserServices.

--- user-microservice/src/main/java/com/aline/usermicroservice/config/BeanConfig.java
+++ user-microservice/src/main/java/com/aline/usermicroservice/config/BeanConfig.java
@@ -3,13 +3,20 @@ package com.aline.usermicroservice.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class MapperConfig {
+public class BeanConfig {
 
     @Bean
     public ModelMapper mapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
