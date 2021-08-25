cb9851054fcf200e16f69cc4c2d906c71797445c
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-25 01:16:10 -0600

    AF-258 Simplified WebSecurityConfig and update core.

--- user-microservice/src/main/java/com/aline/usermicroservice/config/WebSecurityConfig.java
+++ user-microservice/src/main/java/com/aline/usermicroservice/config/WebSecurityConfig.java
@@ -1,24 +1,13 @@
 package com.aline.usermicroservice.config;
 
-import com.aline.core.config.AppConfig;
-import com.aline.core.security.AuthenticationFilter;
-import com.aline.core.security.JwtTokenVerifier;
 import com.aline.core.security.config.AbstractWebSecurityConfig;
-import com.aline.core.security.service.SecurityUserService;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.sql.DataSource;
 
 /**
  * Web Security configuration
  */
 @Configuration
 public class WebSecurityConfig extends AbstractWebSecurityConfig {
-
-    public WebSecurityConfig(DataSource dataSource, PasswordEncoder passwordEncoder, SecurityUserService service, AppConfig appConfig, AuthenticationFilter authenticationFilter, JwtTokenVerifier verifier) {
-        super(dataSource, passwordEncoder, service, appConfig, authenticationFilter, verifier);
-    }
 
     @Override
     public String[] publicAntMatchers() {
