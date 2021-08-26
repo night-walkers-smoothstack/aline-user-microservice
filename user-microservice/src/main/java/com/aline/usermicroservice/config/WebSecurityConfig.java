64ee4eff25642277f3acc21e9b615dc5c11d8360
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-26 15:52:26 -0600

    AF-258 Add UserResponseAuthorizationService

--- user-microservice/src/main/java/com/aline/usermicroservice/config/WebSecurityConfig.java
+++ user-microservice/src/main/java/com/aline/usermicroservice/config/WebSecurityConfig.java
@@ -4,9 +4,7 @@ import com.aline.core.annotations.WebSecurityConfiguration;
 import com.aline.core.security.config.AbstractWebSecurityConfig;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@EnableWebSecurity
 @WebSecurityConfiguration
 public class WebSecurityConfig extends AbstractWebSecurityConfig {
 
